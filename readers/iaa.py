4f18d3b77a04532b0212cfa874c062b771084eea
LUO Yan <lzl257@126.com>
2020-05-27 21:17:41 +0200

    Add more info.

--- readers/iaa.py
+++ readers/iaa.py
@@ -16,12 +16,24 @@ class IAAInvReader(PDFReader):
         return re.findall('Page:\\n\\n(.*?)\\n(.*?)\\n(.*?)\n1', str_content)
     
     
+    def __reg_standard_weight(self, str_content):
+        return re.findall('Weight\\n([0-9]+)\\n\\n(.*?)\\n\\nChargeable', str_content)
+    
+    
     def __reg_weight(self, str_content):
         return re.findall('Chargeable\ weight\\n(.*?)\\n', str_content)
     
     
     def __reg_total(self, str_content):
         return re.findall('Total\ amount\ due\\n\\n(.*?)\\n', str_content)
+    
+    
+    def __reg_awb(self, str_content):
+        return re.findall('Consignee:\\n\\n(.*?)\\n', str_content)
+    
+    
+    def __reg_flight_info(self, str_content):
+        return re.findall('Flight\ No\.:\\n([A-Z]{3}):\\n\\n(.*?)\\n(.*?)\\n(.*?)\\n(.*?)\\n(.*?)\\n\\nInvoice', str_content)
 
     
     def get_info(self):
@@ -41,10 +53,14 @@ class IAAInvReader(PDFReader):
                                                   "Chargeable weight", "Total amount due"]))
                     
                     cap_basic = self.__reg_basic(text_str)
+                    cap_st_weight = self.__reg_standard_weight(text_str)
                     cap_weight = self.__reg_weight(text_str)
                     cap_total = self.__reg_total(text_str)
+                    cap_awb = self.__reg_awb(text_str)
+                    cap_flight = self.__reg_flight_info(text_str)
 
-                    cap_dict = {"Basic info": cap_basic, "Weight info": cap_weight, "Credit info": cap_total}
+                    cap_dict = {"Basic info": cap_basic, "Standard weight info": cap_st_weight,"Chargeable weight info": cap_weight, 
+                             "Credit info": cap_total, "AWB": cap_awb, "Flight info": cap_flight}
 
                     df_dict = {}
                     for key, cap_content in cap_dict.items():                
@@ -54,8 +70,16 @@ class IAAInvReader(PDFReader):
                                 df_dict["Invoice number"] = cap_value[0]
                                 df_dict["Invoice date"] = cap_value[1]
                                 df_dict["Invoice due date"] =  cap_value[2]
-                            elif key == "Weight info":
+                            elif key == "Standard weight info":
+                                df_dict["Weight"] = cap_value[1]
+                            elif key == "Chargeable weight info":
                                 df_dict["Chargeable weight"] = cap_value
+                            elif key == "AWB":
+                                df_dict["AWB"] = cap_value
+                            elif key == "Flight info":
+                                df_dict["Depature"] = cap_value[2]
+                                df_dict["Destination"] = cap_value[3]
+                                df_dict["Time of Depature"] = cap_value[5] + f" ({cap_value[0]})"
                             else:
                                 df_dict["Total amount due"] = cap_value
                         else:
